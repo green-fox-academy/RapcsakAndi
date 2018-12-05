739e53fdfea245bda0af2fee783cd7180309cf78
Rapcsak Andi <escalier81@gmail.com>
2018-12-05 15:13:54 +0100

    equal

--- week-07/equal/main.c
+++ week-07/equal/main.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int equalOrNot(int a, int b);
+
 int main()
 {
     // Create a program which asks for two integers and stores them separatly
     // Create a function which takes two numbers as parameters and
     // returns 1 if they are equal and returns 0 otherwise
 
+    int firstNumber;
+    printf("Enter a number:\n");
+    scanf("%d", &firstNumber);
+    int secondNumber;
+    printf("Enter another number:\n");
+    scanf("%d", &secondNumber);
+
+    int j = equalOrNot(firstNumber, secondNumber);
+
+    printf("%d", j);
+
+
     return 0;
+}
+
+int equalOrNot(int a, int b){
+    int c = 2;
+    if (a == b){
+         c = 1;
+    }else
+         c = 0;
+    return c;
 }
